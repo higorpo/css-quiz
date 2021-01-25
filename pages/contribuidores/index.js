4c7f8f94215e508e78fb6c2cc90197b85aed0cd1
Higor <higorpiresoliveira@gmail.com>
2021-01-25 10:11:27 -0300

    :sparkles: Update logo and background

--- pages/contribuidores/index.js
+++ pages/contribuidores/index.js
@@ -11,14 +11,14 @@ import GitHubCorner from '../../src/components/GitHubCorner'
 
 
 export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
+    width: 100%;
+    max-width: 350px;
+    padding-top: 45px;
+    margin: auto 10%;
+    @media screen and (max-width: 500px) {
+        margin: auto;
+        padding: 15px;
+    }
 `;
 
 function Image({ src, indice }) {
