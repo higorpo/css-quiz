5dee336a147149abcfbccb8ca1d651e648545538
Higor <higorpiresoliveira@gmail.com>
2021-01-25 09:43:45 -0300

    :hammer: Update contributors

--- pages/index.js
+++ pages/index.js
@@ -25,29 +25,29 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-  return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+    return (
+        <QuizBackground backgroundImage={db.bg}>
+            <QuizContainer>
+                <QuizLogo />
+                <Widget>
+                    <Widget.Header>
+                        <h1>{db.title}</h1>
+                    </Widget.Header>
+                    <Widget.Content>
+                        <p>{db.description}</p>
+                    </Widget.Content>
+                </Widget>
 
-        <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
+                <Widget>
+                    <Widget.Content>
+                        <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
-    </QuizBackground>
-  );
+                        <p>lorem ipsum dolor sit amet...</p>
+                    </Widget.Content>
+                </Widget>
+                <Footer />
+            </QuizContainer>
+            <GitHubCorner projectUrl="https://github.com/omariosouto" />
+        </QuizBackground>
+    );
 }
