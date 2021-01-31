b08aab841ffb940e54de882336e92526f7f5431a
Higor <higorpiresoliveira@gmail.com>
2021-01-31 14:33:06 -0300

    :sparkles: Add 'Quizes da galera'

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
-import db from '../db.json';
+import db from '../db.json'
 import Widget from '../src/components/Widget'
+import Link from '../src/components/Link'
 import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
-import Input from '../src/components/Input';
-import Button from '../src/components/Button';
+import Input from '../src/components/Input'
+import Button from '../src/components/Button'
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -75,7 +76,26 @@ export default function Home() {
                         <Widget.Content>
                             <h1>Quizes da Galera</h1>
 
-                            <p>lorem ipsum dolor sit amet...</p>
+                            <ul>
+                                {db.external.map((linkExterno) => {
+                                    const [projectName, githubUser] = linkExterno
+                                        .replace(/\//g, '')
+                                        .replace('https:', '')
+                                        .replace('.vercel.app', '')
+                                        .split('.');
+
+                                    return (
+                                        <li key={linkExterno}>
+                                            <Widget.Topic
+                                                as={Link}
+                                                href={`/quiz/${projectName}___${githubUser}`}
+                                            >
+                                                {`${githubUser}/${projectName}`}
+                                            </Widget.Topic>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
                         </Widget.Content>
                     </Widget>
                     <Footer />
--- src/screens/Quiz/index.js
+++ src/screens/Quiz/index.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import db from '../db.json';
-import Widget from '../src/components/Widget';
-import QuizLogo from '../src/components/QuizLogo';
-import QuizBackground from '../src/components/QuizBackground';
-import QuizContainer from '../src/components/QuizContainer';
-import AlternativesForm from '../src/components/AlternativesForm';
-import Button from '../src/components/Button';
+import Widget from '../../components/Widget';
+import QuizLogo from '../../components/QuizLogo';
+import QuizBackground from '../../components/QuizBackground';
+import QuizContainer from '../../components/QuizContainer';
+import AlternativesForm from '../../components/AlternativesForm';
+import Button from '../../components/Button';
+import BackLinkArrow from '../../components/BackLinkArrow';
 
 function ResultWidget({ results }) {
     return (
@@ -79,7 +79,7 @@ function QuestionWidget({
     return (
         <Widget>
             <Widget.Header>
-                {/* <BackLinkArrow href="/" /> */}
+                <BackLinkArrow href="/" />
                 <h3>
                     {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
                 </h3>
@@ -159,14 +159,15 @@ const screenStates = {
     LOADING: 'LOADING',
     RESULT: 'RESULT',
 };
-export default function QuizPage() {
+export default function QuizPage({ externalQuestions, externalBg }) {
     const [screenState, setScreenState] = React.useState(screenStates.LOADING);
     const [results, setResults] = React.useState([]);
 
-    const totalQuestions = db.questions.length;
     const [currentQuestion, setCurrentQuestion] = React.useState(0);
     const questionIndex = currentQuestion;
-    const question = db.questions[questionIndex];
+    const question = externalQuestions[questionIndex];
+    const totalQuestions = externalQuestions.length;
+    const bg = externalBg;
 
     React.useEffect(() => {
         setTimeout(() => {
@@ -192,7 +193,7 @@ export default function QuizPage() {
     }
 
     return (
-        <QuizBackground backgroundImage={db.bg}>
+        <QuizBackground backgroundImage={bg}>
             <QuizContainer>
                 <QuizLogo />
                 {screenState === screenStates.QUIZ && (
