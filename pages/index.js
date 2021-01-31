56a5feb7ed4a15bd393bb386483cd122aaf318c4
Higor <higorpiresoliveira@gmail.com>
2021-01-31 14:43:20 -0300

    :sparkles: Add animations

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
+
 import db from '../db.json'
 import Widget from '../src/components/Widget'
 import Link from '../src/components/Link'
@@ -51,7 +53,22 @@ export default function Home() {
             <QuizBackground backgroundImage={db.bg}>
                 <QuizContainer>
                     <QuizLogo />
-                    <Widget>
+                    <Widget
+                        as={motion.section}
+                        transition={{ delay: 0, duration: 0.5 }}
+                        variants={{
+                            show: {
+                                opacity: 1,
+                                y: '0'
+                            },
+                            hidden: {
+                                opacity: 0,
+                                y: '100%'
+                            }
+                        }}
+                        initial="hidden"
+                        animate="show"
+                    >
                         <Widget.Header>
                             <h1>{db.title}</h1>
                         </Widget.Header>
@@ -72,7 +89,20 @@ export default function Home() {
                         </Widget.Content>
                     </Widget>
 
-                    <Widget>
+                    <Widget
+                        as={motion.section}
+                        transition={{ delay: 0.5, duration: 0.5 }}
+                        variants={{
+                            show: {
+                                opacity: 1,
+                            },
+                            hidden: {
+                                opacity: 0
+                            }
+                        }}
+                        initial="hidden"
+                        animate="show"
+                    >
                         <Widget.Content>
                             <h1>Quizes da Galera</h1>
 
@@ -98,7 +128,20 @@ export default function Home() {
                             </ul>
                         </Widget.Content>
                     </Widget>
-                    <Footer />
+                    <Footer
+                        as={motion.footer}
+                        transition={{ delay: 1, duration: 0.5 }}
+                        variants={{
+                            show: {
+                                opacity: 1,
+                            },
+                            hidden: {
+                                opacity: 0
+                            }
+                        }}
+                        initial="hidden"
+                        animate="show"
+                    />
                 </QuizContainer>
                 <GitHubCorner projectUrl="https://github.com/higorpo/css-quiz" />
             </QuizBackground>
